88a5c081eaeb316ed0db866955b5085a9206b57d
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-10 15:50:06 -0800

    gigig

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: this is firstc program
+ *
+ * Return: Always Return 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+	if (n > 0)
+	printf("%d is positive/n");
+	else if (n == 0)
+	printf("%d is zero/n");
+	else
+	printf("%d is negative/n");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there*/
+	
+	int m;
+
+	m = n % 10;
+	if(m > 5)
+		printf("last digit of %d is %d and is greater than 5\n" n,m);
+	if (m == 0)
+		print("Last digit of %d is %d and is 0\n", n, m);
+	if (m < 6 && m != 0)
+		printf("Last digit of %d is %d and less than 6 not 0\n" , n, m)
