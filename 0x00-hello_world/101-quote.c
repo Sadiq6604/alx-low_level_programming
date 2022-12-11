1c0fe6a83124c2d216cac9b103fa4493595cb3b5
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-11 04:31:45 -0800

    task one wahala

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * *Return Always 1
+ * Return: Always 1
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
