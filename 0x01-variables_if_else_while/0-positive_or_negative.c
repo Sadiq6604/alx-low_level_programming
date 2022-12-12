72af2fd077124bcb164b49e9c1336975f176d8a0
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-12 14:45:01 -0800

    task positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -11,6 +13,9 @@ int main(void)
 {
 
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
