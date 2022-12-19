11fd75cac91d26c514f4a6c034701b7421c9f5cc
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-19 06:53:03 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _strein - func return the lenght string
+ * @s: this is the input string
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0;  s[index] != '\0' ; index++)
+
+		return (index);
+}
