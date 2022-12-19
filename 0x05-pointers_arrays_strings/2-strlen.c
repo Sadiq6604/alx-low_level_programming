b3655a2b01e18adcac45eef7ad75ab3bfe8ea5ee
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-19 14:59:20 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /*
- * _strein - func return the lenght string
+ * _strlen - function that return the length string
+ *
  * @s: this is the input string
- * Return: lenght of the string
+ *
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
 	int index;
 
-	for (index = 0;  s[index] != '\0' ; index++)
+for (index = 0;  s[index] != '\0' ; index++);
 
 		return (index);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _put - write a function that prints a string, followed by new line, to
+ * stdout.
+ * @str: this is my input string
+ *
+ */
+void _put(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
