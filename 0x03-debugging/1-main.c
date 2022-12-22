8e3e303cfdf59ce09e8d1f876b0c9560ffe6f4b2
Sadiq6604 <saddiqumukhtar67@gmail.com>
2022-12-22 14:41:10 -0800

    0-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>                                                                                                                            
+
+                                                                                                                                              
+
+/**                                                                                                                                           
+ *
+ *  * main - causes an infinite loop                                                                                                             
+ *
+ *   * Return:0                                                                                                                                   
+ *
+ *    */                                                                                                                                           
+
+                                                                                                                                              
+
+int main(void)                                                                                                                                
+
+{                                                                                                                                             
+
+	        int i;                                                                                                                                
+
+		                                                                                                                                              
+
+		        printf("print loop incoming :(\n");                                                                                                   
+
+			                                                                                                                                              
+
+			        i = 0;                                                                                                                                
+
+				        /**                                                                                                                                   
+					 *
+					 *          * while (i < 10)                                                                                                                     
+					 *
+					 *                   * {                                                                                                                                  
+					 *
+					 *                            *     putchar(i);                                                                                                                    
+					 *
+					 *                                     * }                                                                                                                                  
+					 *
+					 *                                              */                                                                                                                                   
+
+				        printf("infinite loop avoided: \\o/\");                                                                                               
+
+						                                                                                                                                              
+
+}                                                                                                                                             
+
+                    
