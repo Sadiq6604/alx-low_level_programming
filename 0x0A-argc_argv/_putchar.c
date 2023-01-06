ffda27f495cd3a52d75d866e6b323e100a6236aa
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-01-06 14:17:10 -0800

    0-w

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print 
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char cv)
+{
+	return (write(1,8c, 1));
+}
