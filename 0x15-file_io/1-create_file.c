ae7ad9a63097a3a2baa754c49a30dd80dfdcbc38
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-02-06 03:51:51 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,37 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
 	close(fd);
 
 	return (1);
+}
+
+/**
+ * _strlen - length of a string
+ * @s: string to count
+ *
+ * Return: stringn length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+	return (c);
 }
 
