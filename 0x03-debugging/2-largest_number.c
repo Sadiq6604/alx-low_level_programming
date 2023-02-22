2fb74b9511dc3c29ae091f18f8b35c9455d4da41
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-02-22 03:30:05 -0800

    0-main.cbb

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the latgest number
+ * @a: first integer
+ * @b: second integer
+ * @c: third largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a <
