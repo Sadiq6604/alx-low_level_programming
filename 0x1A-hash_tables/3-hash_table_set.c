4d66e500e8772d4e5652fc2f066b049aa9fe61f5
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-04-07 06:34:20 -0700

    finally

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,79 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_set - add an element to the table
-* @ht: hash table
-* @key: key of the element
-* @value: value of the element
-* Return: an element added to the table
-*/
+ * hash_table_set - Put a value into hassh tabl3
+ * @ht: The hash table struct
+ * @key: The key used to find the value
+ * @value: The value to add
+ * Return: 1 on success, 0 on fail
+ **/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	char *temporal_value = NULL;
-	hash_node_t *temporal = NULL, *new = NULL;
+	unsigned long int index;
+	hash_node_t *node;
+	int pass_fail;
 
-	if (!ht || !ht->array || !value)
+	if (ht == NULL)
 		return (0);
-
-	if (strlen(key) == 0 || !key)
+	pass_fail = 1;
+	index = key_index((unsigned char *) key, ht->size);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (0);
-	temporal_value = strdup(value);
-	if (!temporal_value)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-
-	
-	temporal = ht->array[index];
-	while (temporal)
+	if (ht->array[index] == NULL)
 	{
-		if (strcmp(temporal->key, key) == 0)
+		ht->array[index] = node;
+		node->key = strdup(key);
+		if (node->key == NULL)
+			return (0);
+		node->value = strdup(value);
+		if (node->value == NULL)
+			return (0);
+		node->next = NULL;
+	}
+	else
+		pass_fail = check_list((char *) key, ht, index, (char *) value);
+	return (pass_fail);
+}
+
+/**
+ * check_list - check a list at the key's index for the key
+ * @key: Key to look for
+ * @ht: Hash table to look through
+ * @index: Index of the hashed key value
+ * @value: The value associated with the key to add
+ * Return: 1 on success, 0 for failure
+ **/
+int check_list(char *key, hash_table_t *ht,
+	       unsigned long int index, char *value)
+{
+	hash_node_t *current_node;
+	hash_node_t *new_node;
+
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(key, current_node->key) == 0)
 		{
-			free(temporal->value);
-			temporal->value = temporal_value;
-			temporal->value = strdup(value);
-			free(temporal_value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
 			return (1);
 		}
-		temporal = temporal->next;
+		current_node = current_node->next;
 	}
-	
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-	{
-		free(new);
+	current_node = ht->array[index];
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-	}
-	new->key = strdup(key);
-	new->value = temporal_value;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new_node->next = current_node;
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+		return (0);
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+		return (0);
+	ht->array[index] = new_node;
 	return (1);
 }
+
