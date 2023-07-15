6665cf11afa87a7e60b472ab2ed2e735645a0959
Sadiq6604 <saddiqumukhtar67@gmail.com>
2023-07-14 22:58:12 -0700

    soo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,40 @@
 #include "search_algos.h"
 
 /**
- * print_search_array - Print the array that's being searched
- * @array: The array being searched
- * @start: Index we want to start at
- * @end: Index to finish at
- **/
-void print_search_array(int *array, int start, int end)
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int recursive_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t half = size / 2;
+	size_t i;
 
-	i = start;
-	printf("Searching in array: ");
-	while (i <= end)
-	{
-		if (i > start)
-			printf(", ");
-		printf("%d", array[i]);
-		i++;
-	}
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
 	printf("\n");
-}
 
-/**
- * binary_search - Use a binary search to find the given value in an array
- * @array: Array to search
- * @size: Size of the given array
- * @value: Value to search for
- * Return: The first index the value appears, or -1 if the value can't be
- * found
- **/
-int binary_search(int *array, size_t size, int value)
-{
-	int left, right, compare;
+	if (half && size % 2 == 0)
+		half--;
 
-	left = compare = 0;
-	right = size - 1;
+	if (value == array[half])
+		return ((int)half);
 
-	while (left <= right)
-	{
-		print_search_array(array, left, right);
-		compare = (left + right) / 2;
-			if (array[compare] == value)
-			return (compare);
-		else if (array[compare] > value)
-		{
-			right = compare;
-		}
-		else if (array[compare] < value)
-		{
-			left = compare + 1;
-		}
-	}
-	return (-1);
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
 }
